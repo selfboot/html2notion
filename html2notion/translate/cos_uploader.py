51b93d096eb3564d200e56f0a62df573474e7511
selfboot <xuezaigds@gmail.com>
2023-04-04 21:05:30 +0800

    add cos async upload and test case

---
+++ html2notion/translate/cos_uploader.py
@@ -0,0 +1,65 @@
+import asyncio
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+from ..utils import logger, test_prepare_conf, config
+from functools import partial
+
+
+class TencentCosUploaderAsync:
+    def __init__(self, secret_id, secret_key, region, bucket):
+        self.config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key)
+        self.client = CosS3Client(self.config)
+        self.bucket = bucket
+
+    async def upload_file(self, loop, local_path, key):
+        with open(local_path, 'rb') as f:
+            content = f.read()
+
+        executor = loop.run_in_executor
+        put_object_partial = partial(self.client.put_object, Bucket=self.bucket, Body=content, Key=key)
+        response = await executor(None, put_object_partial)
+        return response
+
+    async def check_file_exist(self, loop, key):
+        try:
+            executor = loop.run_in_executor
+            return await executor(None, self.client.object_exists, self.bucket, key)
+        except Exception as e:
+            logger.error(e)
+            return False
+
+    async def delete_file(self, loop, key):
+        executor = loop.run_in_executor
+        response = await executor(None, self.client.delete_object, self.bucket, key)
+        return response
+
+
+async def main():
+    test_prepare_conf()
+
+    try:
+        secret_id = config["cos"]["secret_id"]
+        secret_key = config["cos"]["secret_key"]
+        region = config["cos"]["region"]
+        bucket = config["cos"]["bucket"]
+    except Exception as e:
+        print(f"Please fill cos conf in the config file")
+        return
+
+    local_path = './demos/saul.webp'
+    key = 'test/saul.webp'
+
+    uploader = TencentCosUploaderAsync(secret_id, secret_key, region, bucket)
+    loop = asyncio.get_event_loop()
+
+    upload_response = await uploader.upload_file(loop, local_path, key)
+    print(f"Upload response: {upload_response}")
+
+    if await uploader.check_file_exist(loop, key):
+        print("Upload successful!")
+    else:
+        print("Upload failed!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ tests/test_cosupload.py
@@ -0,0 +1,81 @@
+import asyncio
+import pytest
+import time
+import os
+import random
+import string
+from pathlib import Path
+from unittest.mock import patch
+from tempfile import TemporaryDirectory
+from html2notion.translate.batch_import import BatchImport
+from html2notion.translate.cos_uploader import TencentCosUploaderAsync
+
+
+def log_only_local(content):
+    if 'GITHUB_ACTIONS' in os.environ:
+        return
+
+    from html2notion.utils import logger
+    logger.info(content)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare_conf_fixture():
+    if 'GITHUB_ACTIONS' not in os.environ:
+        from html2notion.utils import test_prepare_conf
+        test_prepare_conf()
+        log_only_local("prepare_conf_fixture")
+
+
+async def mock_cos_upload_request(file_path, *args, **kwargs):
+    if 'GITHUB_ACTIONS' not in os.environ:
+        from html2notion.utils import config
+        secret_id = config["cos"]["secret_id"]
+        secret_key = config["cos"]["secret_key"]
+        region = config["cos"]["region"]
+        bucket = config["cos"]["bucket"]
+    else:
+        secret_id = os.environ['secret_id']
+        secret_key = os.environ['secret_key']
+        region = os.environ['region']
+        bucket = os.environ['bucket']
+
+    log_only_local(f"mock_cos_upload_request: {file_path}")
+    uploader = TencentCosUploaderAsync(secret_id, secret_key, region, bucket)
+    loop = asyncio.get_event_loop()
+    key = f"test_workflow/{file_path.name}"
+    upload_response = await uploader.upload_file(loop, file_path, key)
+    log_only_local(f"Upload response: {upload_response}")
+
+    if await uploader.check_file_exist(loop, key):
+        return True
+    else:
+        return False
+
+
+@pytest.fixture()
+def temp_dir_fixture():
+    with TemporaryDirectory() as temp_dir:
+        dir_path = Path(temp_dir)
+        temp_files = []
+        for i in range(100):
+            file_size = random.randint(1 * 1024 * 1024, 5 * 1024 * 1024)
+            random_text = "".join(random.choices(string.ascii_letters + string.digits, k=file_size))
+
+            temp_file = dir_path / f"file_{i}.txt"
+            temp_file.write_text(random_text)
+            temp_files.append(temp_file)
+
+        yield dir_path
+
+
+@pytest.mark.asyncio
+async def test_batch_cos_upload(temp_dir_fixture):
+    concurrent_limit = 10
+    dir_path = temp_dir_fixture
+    with patch("html2notion.translate.notion_import.NotionImporter.process_file", side_effect=mock_cos_upload_request):
+        batch_processor = BatchImport(dir_path, concurrent_limit=concurrent_limit)
+        responses = await batch_processor.process_directory()
+
+    for res in responses:
+        assert (res)
