3150761e28fe776b827122e67fb4bc016ecef11f
selfboot <xuezaigds@gmail.com>
2023-05-20 18:00:59 +0800

    Add clipper test case

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from html2notion.translate.html2json import html2json_process
 from html2notion.translate.import_stats import ImportStats
 from html2notion.translate.html2json_markdown import YinXiangMarkdown_Type
+from html2notion.translate.html2json_clipper import YinXiangClipper_Type
 from html2notion.utils import logger, config
 
 
@@ -14,22 +15,36 @@ def test_demo_files():
     else:
         database_id = config['notion']['database_id']
 
-    # md_files = glob.glob("tests/demo_files/*.html")
-    md_file = Path("./demos/yinxiang_markdown.html")
-    import_stats = ImportStats()
-    notion_data, html_type = html2json_process(md_file, import_stats)
+    testcases = [
+        ["./demos/yinxiang_markdown.html", YinXiangMarkdown_Type, "./demos/yinxiang_markdown.json"],
+        ["./demos/yinxiang_clipper.html", YinXiangClipper_Type, "./demos/yinxiang_clipper.json"],
+        ["./demos/yinxiang_clipper_wx.html", YinXiangClipper_Type, "./demos/yinxiang_clipper_wx.json"],
+    ]
 
-    assert html_type == YinXiangMarkdown_Type
-    with open("./demos/yinxiang_markdown.json", "r") as f:
-        content = f.read()
+    for md_file, expect_type, expect_file in testcases:
+        import_stats = ImportStats()
+        notion_data, html_type = html2json_process(Path(md_file), import_stats)
 
-    content = content.replace("###database_id###", database_id)  # Replace the placeholder
-    expect = json.loads(content)
+        assert html_type == expect_type
+        with open(expect_file, "r") as f:
+            content = f.read()
 
-    # The timezone causes the calculated time to be different, and the check here can be ignored
-    try:
-        del expect['properties']['Created']['date']['start']
-        del notion_data['properties']['Created']['date']['start']
-    except KeyError as e:
-        pass
-    assert notion_data == expect
+        # Replace the placeholder
+        content = content.replace("###database_id###", database_id)
+        expect = json.loads(content)
+
+        # The timezone causes the calculated time to be different, and the check here can be ignored
+        try:
+            del expect['properties']['Created']['date']['start']
+            del notion_data['properties']['Created']['date']['start']
+        except KeyError as e:
+            pass
+        
+        import dictdiffer
+        diff = dictdiffer.diff(notion_data, expect)
+        for d in diff:
+            logger.debug(f'Diff: {d}')
+        # aa = json.dumps(notion_data, ensure_ascii=False)
+        # logger.debug(f'notion_data: {aa}')
+        assert notion_data ==expect 
+
